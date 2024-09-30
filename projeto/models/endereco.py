e4b5d0c70a375dee599283fe664c5587330dc7c4
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-09-30 12:30:53 +0000

    v0.2

---
+++ projeto/models/endereco.py
@@ -0,0 +1,16 @@
+class Endereco:
+    def __init__(self,logradouro:str,numero:str,complemento:str,cep:str,cidade:str) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+
+    def __str__(self) -> str:
+        return(
+            f"\nLogradouro: {self.logradouro}"
+            f"\nNúmero: {self.numero}"
+            f"\nComplemento: {self.complemento}"
+            f"\nCEP: {self.cep}"
+            f"\nCidade: {self.cidade}"
+        )

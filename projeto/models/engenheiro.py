be49d0c5f44a1194a10fd867398d451700c9e9ea
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-09-30 12:45:16 +0000

    v0.6

---
+++ projeto/models/engenheiro.py
@@ -0,0 +1,16 @@
+from projeto.models.endereco import Endereco
+from projeto.models.funcionario import Funcionario
+
+
+class Engenheiro(Funcionario):
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco,crea:str) -> None:
+        super().__init__(nome, telefone, email, endereco)
+        self.crea = crea
+
+    def salario_final(self) -> float:
+        return 10000.0
+    
+    def __str__(self) -> str:
+        return (
+            f"{super().__str__()}"
+            f"\nCREA: {self.crea}")
